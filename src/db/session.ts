32c939ff026e3d097a32415f4f76f439747d1321
Script Raccoon <scriptraccoon@gmail.com>
2023-03-08 01:44:30 +0100

    save session and send cookie after login

---
+++ src/db/session.ts
@@ -0,0 +1,8 @@
+const sessions = new Set(); // should be in a database
+
+export function save_session(): string {
+	const session_id = crypto.randomUUID();
+	sessions.add(session_id);
+	console.log(sessions);
+	return session_id;
+}
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,13 +1,16 @@
 import type { Actions } from "./$types";
 import { SECRET_PASSWORD } from "$env/static/private";
 import { fail } from "@sveltejs/kit";
+import { save_session } from "../../db/session";
 
 export const actions: Actions = {
-	default: async ({ request }) => {
+	default: async ({ request, cookies }) => {
 		const data = await request.formData();
 		const password = data.get("password");
 		const is_correct = password == SECRET_PASSWORD;
 		if (is_correct) {
+			const session_id = save_session();
+			cookies.set("session_id", session_id);
 			return { message: "The password is correct" };
 		} else {
 			return fail(401, {
