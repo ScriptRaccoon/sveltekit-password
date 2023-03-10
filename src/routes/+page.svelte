609836f0f52b6ed6752b2259533a9c354d305f82
Script Raccoon <scriptraccoon@gmail.com>
2023-03-10 20:01:20 +0100

    add github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
 <h1>Home</h1>
 
-<p>This website has a password-protected subpage.</p>
+<p>
+	This website demonstrates how to implement a password-protected
+	subpage inside of a SvelteKit application.
+</p>
+
+<p>
+	You can find the code on <a
+		href="https://github.com/ScriptRaccoon/sveltekit-password"
+		target="_blank"
+		rel="noreferrer">GitHub</a
+	>.
+</p>
